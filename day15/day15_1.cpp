38f57750cf2c8a6daeec30fe687d7ac91c0278a7
Evita Overtoom <eovertoo@f0r4s17.codam.nl>
2022-03-08 11:57:38 +0100

    removed trash

--- day15/day15_1.cpp
+++ day15/day15_1.cpp
@@ -6,12 +6,12 @@
 #include <algorithm>
 
 struct  data {
-    int x,y,cost,cnt;
+    int x,y,cost,manhat;
 };
 
 struct  CompareData{
     bool    operator()(data *a, data *b){
-        return a->cost > b->cost;
+        return (a->cost+a->manhat) > (b->cost+b->manhat);
     }
 };
 
@@ -23,65 +23,58 @@ void        readfile(char *file, std::vector<std::string> &grid){
     f.close();
 }
 
+int         manHattan(int x, int y, int size){
+    int xman = abs(size - x);
+    int yman = abs(size - y);
+    return xman+yman;
+}
+
+data*       addNode(int x, int y, std::vector<std::string> &grid, data *tmp){
+    data    *a = new data();
+    a->x = x;
+    a->y = y;
+    a->cost = grid[y][x]-48 + tmp->cost;
+    a->manhat = manHattan(a->x, a->y, grid.size()-1);
+    return a;
+}
+
+void        addToOpen(data *newElem, int s, std::priority_queue<data*, std::vector<data*>, CompareData> &open, std::map<int, int> &closed){
+    int x = newElem->y*s+newElem->x;
+    auto it = closed.find(x);
+    if (it == closed.end()){
+        closed.insert(std::pair<int,int>(x, newElem->cost));
+        open.push(newElem);
+    }
+    else if (it->second > newElem->cost){
+        closed[x] = newElem->cost;
+        open.push(newElem);
+    }
+}
+
 void        searchpath(std::vector<std::string> &grid, std::priority_queue<data*, std::vector<data*>, CompareData> &open, std::map<int, int> &closed){
     while(!open.empty()){
         data    *tmp = open.top();
         open.pop();
-        if (tmp->y + 1 == grid.size() && tmp->x + 1 == grid[0].size()){
-            std::cout << tmp->cnt << std::endl;
+        data    *newElem;
+        if (tmp->x != 0 && tmp->manhat == 0){
+            std::cout << tmp->cost << std::endl;
             return;
         }
-        if (tmp->x + 1 < grid[tmp->y].size()){
-            data    *a = new data();
-            a->x = tmp->x + 1;
-            a->y = tmp->y;
-            a->cost = tmp->cost + 1 + (grid[a->y][a->x]-48);
-            a->cnt = tmp->cnt + (grid[a->y][a->x]-48);
-            int x = a->y*grid.size()+a->x;
-            auto it = closed.find(x);
-            if (it == closed.end()){
-                closed.insert(std::pair<int,int>(a->y*grid.size()+a->x, a->cost));
-                open.push(a);
-            }
+        if (tmp->x + 1 < grid.size()){
+            newElem = addNode(tmp->x+1, tmp->y, grid, tmp);
+            addToOpen(newElem, grid.size(), open, closed);
         }
         if (tmp->x - 1 >= 0){
-            data    *b = new data();
-            b->x = tmp->x - 1;
-            b->y = tmp->y;
-            b->cost = tmp->cost + 1 + (grid[b->y][b->x]-48);
-            b->cnt = tmp->cnt + (grid[b->y][b->x]-48);
-            int x = b->y*grid.size()+b->x;
-            auto it = closed.find(x);
-            if (it == closed.end()){
-                closed.insert(std::pair<int,int>(b->y*grid.size()+b->x, b->cost));
-                open.push(b);
-            }
+            newElem = addNode(tmp->x-1, tmp->y, grid, tmp);
+            addToOpen(newElem, grid.size(), open, closed);
         }
         if (tmp->y + 1 < grid.size()){
-            data    *c = new data();
-            c->x = tmp->x;
-            c->y = tmp->y + 1;
-            c->cost = tmp->cost + 1 + (grid[c->y][c->x]-48);
-            c->cnt = tmp->cnt + (grid[c->y][c->x]-48);
-            int x = c->y*grid.size()+c->x;
-            auto it = closed.find(x);
-            if (it == closed.end()){
-                closed.insert(std::pair<int,int>(c->y*grid.size()+c->x, c->cost));
-                open.push(c);
-            }
+            newElem = addNode(tmp->x, tmp->y+1, grid, tmp);
+            addToOpen(newElem, grid.size(), open, closed);
         }
         if (tmp->y - 1 >= 0){
-            data    *d = new data();
-            d->x = tmp->x;
-            d->y = tmp->y - 1;
-            d->cost = tmp->cost + 1 + (grid[d->y][d->x]-48);
-            d->cnt = tmp->cnt + (grid[d->y][d->x]-48);
-            int x = d->y*grid.size()+d->x;
-            auto it = closed.find(x);
-            if (it == closed.end()){
-                closed.insert(std::pair<int,int>(d->y*grid.size()+d->x, d->cost));
-                open.push(d);
-            }
+            newElem = addNode(tmp->x, tmp->y-1, grid, tmp);
+            addToOpen(newElem, grid.size(), open, closed);
         }
     }
 }
@@ -97,7 +90,7 @@ int         main(int argc, char **argv){
     init.x = 0;
     init.y = 0;
     init.cost = 0;
-    init.cnt = 0;
+    init.manhat = 0;
     closed.insert(std::pair<int,int>(0,0));
     open.push(&init);
     searchpath(grid, open, closed);
